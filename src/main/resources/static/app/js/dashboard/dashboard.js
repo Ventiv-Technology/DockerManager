58336012c68508b8407f5d926097cf1717309955
John Crygier <john.crygier@ventivtech.com>
2015-03-27 15:49:27 -0500

    Upgrade to the working ui-sortable.  Add default structures for dashboard.

--- src/main/resources/static/app/js/dashboard/dashboard.js
+++ src/main/resources/static/app/js/dashboard/dashboard.js
@@ -114,4 +114,94 @@ define(['angular', 'vendor/angular-dashboard-framework/0.7.0/angular-dashboard-f
             }
         })
 
+        .config(function(dashboardProvider){
+            dashboardProvider
+                .structure('6-6', {
+                    rows: [{
+                        columns: [{
+                            styleClass: 'col-md-6'
+                        }, {
+                            styleClass: 'col-md-6'
+                        }]
+                    }]
+                })
+                .structure('4-8', {
+                    rows: [{
+                        columns: [{
+                            styleClass: 'col-md-4',
+                            widgets: []
+                        }, {
+                            styleClass: 'col-md-8',
+                            widgets: []
+                        }]
+                    }]
+                })
+                .structure('12/4-4-4', {
+                    rows: [{
+                        columns: [{
+                            styleClass: 'col-md-12'
+                        }]
+                    }, {
+                        columns: [{
+                            styleClass: 'col-md-4'
+                        }, {
+                            styleClass: 'col-md-4'
+                        }, {
+                            styleClass: 'col-md-4'
+                        }]
+                    }]
+                })
+                .structure('12/6-6', {
+                    rows: [{
+                        columns: [{
+                            styleClass: 'col-md-12'
+                        }]
+                    }, {
+                        columns: [{
+                            styleClass: 'col-md-6'
+                        }, {
+                            styleClass: 'col-md-6'
+                        }]
+                    }]
+                })
+                .structure('12/6-6/12', {
+                    rows: [{
+                        columns: [{
+                            styleClass: 'col-md-12'
+                        }]
+                    }, {
+                        columns: [{
+                            styleClass: 'col-md-6'
+                        }, {
+                            styleClass: 'col-md-6'
+                        }]
+                    }, {
+                        columns: [{
+                            styleClass: 'col-md-12'
+                        }]
+                    }]
+                })
+                .structure('3-9 (12/6-6)', {
+                    rows: [{
+                        columns: [{
+                            styleClass: "col-md-3"
+                        }, {
+                            styleClass: "col-md-9",
+                            rows: [{
+                                columns: [{
+                                    styleClass: "col-md-12"
+                                }]
+                            }, {
+                                columns: [{
+                                    styleClass: "col-md-6"
+                                }, {
+                                    styleClass: "col-md-6"
+                                }]
+                            }]
+                        }]
+                    }]
+                });
+
+        });
+
 });
