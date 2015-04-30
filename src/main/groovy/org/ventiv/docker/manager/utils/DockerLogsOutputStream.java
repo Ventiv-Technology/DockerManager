cfbb42649fcc3646ecd6682cc4020b0494021b63
John Crygier <john.crygier@ventivtech.com>
2015-04-30 12:51:25 -0500

    Update license, fix TODO comment.

--- src/main/groovy/org/ventiv/docker/manager/utils/DockerLogsOutputStream.java
+++ src/main/groovy/org/ventiv/docker/manager/utils/DockerLogsOutputStream.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (c) 2014 - 2015 Ventiv Technology
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package org.ventiv.docker.manager.utils;
 
 import java.io.IOException;
@@ -37,7 +52,7 @@ public class DockerLogsOutputStream extends OutputStream {
                 b[off+1] == 0 &&
                 b[off+2] == 0 &&
                 b[off+3] == 0) {
-            // TODO: If we ever care, extract header information: b[off] == 0 -> Stdin, b[off] == 1 -> Stdout, b[off] == 2 -> Stderr
+            // If we ever care, extract header information: b[off] == 0 -> Stdin, b[off] == 1 -> Stdout, b[off] == 2 -> Stderr
             if (prependStreamName) {
                 byte[] toPrepend = STD_IN;
                 if (b[off] == 0x1) toPrepend = STD_OUT;
