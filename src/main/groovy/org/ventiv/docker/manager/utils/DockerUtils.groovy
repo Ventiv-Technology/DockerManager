5a20b06fd9714d15df8f62b716352178f283ced1
asheibani <amir.sheibani@ventivtech.com>
2015-09-04 09:04:59 -0400

    return new Date(0L) instead of an exception

--- src/main/groovy/org/ventiv/docker/manager/utils/DockerUtils.groovy
+++ src/main/groovy/org/ventiv/docker/manager/utils/DockerUtils.groovy
@@ -66,7 +66,7 @@ class DockerUtils {
             periodType = approxMatcher[0][1].toLowerCase() + 's';
         }
 
-        if (periodType == null) return null
+        if (periodType == null) return new Date(0L)
            // throw new IllegalArgumentException("DockerUtils doesn't know how to translate ps status of '${psStatus}' to a date.  Please add to DockerUtilsTest.\"can convert 'docker ps' status to status date\".")
 
         return dateTime.minus(Period."$periodType"(scalar)).toDate();
