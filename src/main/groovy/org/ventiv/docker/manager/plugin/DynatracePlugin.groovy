cf7493558353208591bdeae120ed8d9a7f3621d1
John Crygier <john.crygier@ventivtech.com>
2016-12-13 12:11:12 -0600

    Add ability to override host name for Dynatrace Plugin

--- src/main/groovy/org/ventiv/docker/manager/plugin/DynatracePlugin.groovy
+++ src/main/groovy/org/ventiv/docker/manager/plugin/DynatracePlugin.groovy
@@ -23,23 +23,32 @@ import org.ventiv.docker.manager.model.ServiceInstance
  */
 class DynatracePlugin implements CreateContainerPlugin {
 
-    public static final DYNATRACE_PATH          = "DYNATRACE_PATH";
-    public static final DYNATRACE_SERVER        = "DYNATRACE_SERVER";
-    public static final DYNATRACE_NAME          = "DYNATRACE_NAME";
-    public static final JAVA_OPTS               = "JAVA_OPTS";
+    public static final DYNATRACE_PATH                  = "DYNATRACE_PATH";
+    public static final DYNATRACE_SERVER                = "DYNATRACE_SERVER";
+    public static final DYNATRACE_NAME                  = "DYNATRACE_NAME";
+    public static final DYNATRACE_OVERRIDE_HOST_NAME    = "DYNATRACE_OVERRIDE_HOST_NAME";
+    public static final JAVA_OPTS                       = "JAVA_OPTS";
+
+    public static final CONTAINER_NAME                  = "CONTAINER_NAME";
 
     @Override
     void doWithServiceInstance(ServiceInstance serviceInstance) {
         Map<String, String> env = serviceInstance.getResolvedEnvironmentVariables();
 
         if (env.containsKey(DYNATRACE_PATH) && env.containsKey(DYNATRACE_SERVER) && env.containsKey(DYNATRACE_NAME)) {
-            String dynaTraceAgent = " -agentpath:${env[DYNATRACE_PATH]}=name=${env[DYNATRACE_NAME]},server=${env[DYNATRACE_SERVER]}"
+            // Derive the host name - First use DYNATRACE_OVERRIDE_HOST_NAME Env variable, then the server name of teh service instance.  If either of those are 'CONTAINER_NAME' - change to 12 characters of the ID
+            String overrideHostName = ",overridehostname=" + (env[DYNATRACE_OVERRIDE_HOST_NAME] ?: serviceInstance.serverName);
+            if (env[DYNATRACE_OVERRIDE_HOST_NAME] == CONTAINER_NAME)
+                overrideHostName = "";
+
+            String dynaTraceAgent = " -agentpath:${env[DYNATRACE_PATH]}=name=${env[DYNATRACE_NAME]},server=${env[DYNATRACE_SERVER]}${overrideHostName}"
             env.put(JAVA_OPTS, env[JAVA_OPTS] ? env[JAVA_OPTS] + dynaTraceAgent : dynaTraceAgent);
 
             // Now, we don't need the DYNATRACE_*** variables, lets clean em up
             env.remove(DYNATRACE_PATH)
             env.remove(DYNATRACE_SERVER)
             env.remove(DYNATRACE_NAME)
+            env.remove(DYNATRACE_OVERRIDE_HOST_NAME)
         }
     }
 
